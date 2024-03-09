6929ec2dfac7961f7d4f0a6fb7aa3844e5c1632a
u-gun <131837352+u-gun@users.noreply.github.com>
2024-03-10 07:53:19 +0900

    Morse.cpp 업데이트

--- C++/Morse/Morse.cpp
+++ C++/Morse/Morse.cpp
@@ -6,19 +6,19 @@
 
 class Morse {
 	std::map<std::queue<bool>, int> alphabet;
-        std::map<std::queue<bool>, int> digit;
+ std::map<std::queue<bool>, int> digit;
 	std::map<std::queue<bool>, char[2]> hangul;
-  string slash, question, comma, period, plus, equal;
+ string slash, question, comma, period, plus, equal;
 
 	/**
- 	* Morse cord를 word 단위로 저장
-  	* dit=1 dash=111 시그널 간격=0 
-  	*/
-	std::queue<bool> word_q
+  * Morse cord를 word 단위로 저장
+ 	* dit=1 dash=111 signel interval=0 letter interval=000
+  */
+	std::queue<bool> morseWord_q
   
 public :
 	Morse();
-	telegraph
+	moseWord_q telegraph();
 	void text2Morse(string text, string& morse);
 	bool morse2Text(string morse, string& text);
 }
